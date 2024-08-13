6777051073d93171539d1fcb8fca737df8d2699a
LucetTin5 <Lucettin5@gmail.com>
2024-08-13 20:43:31 +0900

    chore: lighthouse 테스트에서 PWA 속성 제외

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,7 +3,16 @@ module.exports = {
     collect: {
       staticDistDir: './',
       startServerCommand: 'http-server . -p 8080',
-      url: ['http://localhost:8080']
+      url: ['http://localhost:8080'],
+      settings: {
+        onlyCategories: [
+          'performance',
+          'accessibility',
+          'best-practices',
+          'seo',
+        ],
+        skipAudits: ['installable-manifest', 'service-worker'],
+      },
     },
     upload: {
       target: 'temporary-public-storage',
